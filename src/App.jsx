6f1d460a01e9ad285fd0c8e5c8189eace639958c
Himanshu Kumar <dhoni07hk@gmail.com>
2023-03-04 20:56:52 +0530

    little fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { fetchDataFromApi } from "./utils/api";
